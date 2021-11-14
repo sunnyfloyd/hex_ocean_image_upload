485b1465fe6d50cb8d94d8078ffba8f76d14c48b
sunnyfloyd <pain.floyd.maq@gmail.com>
2021-11-14 21:00:14 +0100

    default project setup command

--- image_hoarder/images/management/commands/set_up_default_plans.py
+++ image_hoarder/images/management/commands/set_up_default_plans.py
@@ -7,43 +7,47 @@ class Command(BaseCommand):
     help = "Sets up a default plans and thumbnail options."
 
     def handle(self, *args, **options):
-        # Create thumbnails
-        tn200 = ThumbnailOption.objects.create(height=200)
-        tn400 = ThumbnailOption.objects.create(height=400)
-
-        # Create plans
-        plan_basic = Plan.objects.create(
-            name="Basic", keep_original=False, has_expiry_link=False
-        )
-        plan_basic.thumbnail_options.add(tn200)
-
-        plan_premium = Plan.objects.create(
-            name="Premium", keep_original=True, has_expiry_link=False
-        )
-        plan_premium.thumbnail_options.add(tn200, tn400)
-
-        plan_enterprise = Plan.objects.create(
-            name="Enterprise", keep_original=True, has_expiry_link=True
-        )
-        plan_enterprise.thumbnail_options.add(tn200, tn400)
-
-        # Create users
-        User.objects.create_superuser(
-            username="admin", password="123", plan=plan_enterprise
-        )
+        try:
+            User.objects.get(username="admin")
         
-        User.objects.create_user(
-            username="basic", password="123", plan=plan_basic
-        )
-        User.objects.create_user(
-            username="premium", password="123", plan=plan_premium
-        )
-        User.objects.create_user(
-            username="enterprise", password="123", plan=plan_enterprise
-        )
+        except User.DoesNotExist:
+            # Create thumbnails
+            tn200 = ThumbnailOption.objects.create(height=200)
+            tn400 = ThumbnailOption.objects.create(height=400)
 
-        self.stdout.write(
-            self.style.SUCCESS(
-                "Default plans have been successfully created."
+            # Create plans
+            plan_basic = Plan.objects.create(
+                name="Basic", keep_original=False, has_expiry_link=False
             )
-        )
+            plan_basic.thumbnail_options.add(tn200)
+
+            plan_premium = Plan.objects.create(
+                name="Premium", keep_original=True, has_expiry_link=False
+            )
+            plan_premium.thumbnail_options.add(tn200, tn400)
+
+            plan_enterprise = Plan.objects.create(
+                name="Enterprise", keep_original=True, has_expiry_link=True
+            )
+            plan_enterprise.thumbnail_options.add(tn200, tn400)
+
+            # Create users
+            User.objects.create_superuser(
+                username="admin", password="123", plan=plan_enterprise
+            )
+            
+            User.objects.create_user(
+                username="basic", password="123", plan=plan_basic
+            )
+            User.objects.create_user(
+                username="premium", password="123", plan=plan_premium
+            )
+            User.objects.create_user(
+                username="enterprise", password="123", plan=plan_enterprise
+            )
+
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "Default plans have been successfully created."
+                )
+            )
